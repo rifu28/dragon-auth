e84297fda57e32aa459ab6b04ef1691bbc660ea2
rifu28 <rifatmeumeu28@gmail.com>
2024-09-02 23:55:38 +0600

    already done

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div>Hello world! welcome to our home</div>,
   },
 ]);
 
