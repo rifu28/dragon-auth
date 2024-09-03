d2fe04502c2119976106ac10c646023b7b1d7e25
rifu28 <rifatmeumeu28@gmail.com>
2024-09-04 02:10:06 +0600

    golapi golapi

--- src/BreakingNews.jsx
+++ src/BreakingNews.jsx
@@ -1,7 +1,30 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
 const BreakingNews = () => {
   return (
-    <div>
-      <button className="btn btn-secondary">Latest News</button>
+    <div className="flex my-7 items-center bg-gray-100 px-3 py-2">
+      <div>
+        <button className="bg-pink-500 font-semibold border-2 border-pink-200 text-white px-3 py-1">
+          Latest
+        </button>
+      </div>
+      <div>
+        <Marquee pauseOnHover={true}>
+          <Link to="/" className="mr-12 font-semibold">
+            I can be a React component, multiple React components, or just some
+            text.
+          </Link>
+          <Link to="/" className="mr-12 font-semibold">
+            I can be a React component, multiple React components, or just some
+            text.
+          </Link>
+          <Link to="/" className="mr-12 font-semibold">
+            I can be a React component, multiple React components, or just some
+            text.
+          </Link>
+        </Marquee>
+      </div>
     </div>
   );
 };
