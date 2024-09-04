a4e59a989da7416bc524cc669c96b1681fa9220e
rifu28 <rifatmeumeu28@gmail.com>
2024-09-04 10:01:23 +0600

    right nav

--- src/RightNav.jsx
+++ src/RightNav.jsx
@@ -1,7 +1,19 @@
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
 const RightNav = () => {
   return (
-    <div>
-      <h2 className="text-2xl font-semibold">Right side nav</h2>
+    <div className="px-3 py-2 space-y-3">
+      <h2 className="text-2xl font-semibold">Login with</h2>
+
+      <button className="btn btn-outline w-full">
+        {" "}
+        <FaGoogle></FaGoogle>Google
+      </button>
+      <button className="btn btn-outline w-full">
+        {" "}
+        <FaGithub></FaGithub>Github
+      </button>
     </div>
   );
 };
