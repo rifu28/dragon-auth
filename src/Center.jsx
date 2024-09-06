e4e00586dafc13cc93dd44c63a1b56c06be20e1b
rifu28 <rifatmeumeu28@gmail.com>
2024-09-06 16:23:05 +0600

    center

--- src/Center.jsx
+++ src/Center.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Center = () => {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    fetch("news.json")
+      .then((result) => result.json())
+      .then((data) => setNews(data));
+  }, []);
   return (
     <div className="py-2">
-      <h2 className="text-2xl font-semibold">Center position</h2>
+      <h2 className="text-2xl font-semibold">Center position :{news.length}</h2>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
 import AuthProvider from "./AuthProvider";
+import Center from "./Center";
 const router = createBrowserRouter([
   {
     path: "/",
