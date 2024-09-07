8cf4843ad86057edb1b9a8083f7ffeeecb5ada8e
rifu28 <rifatmeumeu28@gmail.com>
2024-09-07 19:51:00 +0600

    another update

--- src/Login.jsx
+++ src/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
       <Navbar></Navbar>
       <div className="w-1/2 mx-auto mt-10 bg-gray-50 p-3">
         <h2 className="text-2xl font-semibold text-center mb-5 mt-3">
-          Login Your Account
+          LogIn Your Account
         </h2>
         <hr />
         <form onSubmit={handleLogin} className="card-body">
